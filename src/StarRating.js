78cd1ada34f1b8182889b2c89fb753d52b72e3b0
mrsherpacode <sherpacoding@gmail.com>
2025-03-24 15:49:47 -0500

    continuous update

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,26 +10,30 @@ const starContainer = {
 const star = {
   display: "flex",
 };
-const text = {
-  lineHeight: "1",
-  margin: "0",
-};
 
 // StarRating component //
-// starStyle //
-const starStyle = {
-  width: "40px",
-  height: "40px",
-  display: "block",
-  curser: "Pointer",
-};
 
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const text = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
-  // handleSubmit //
+
   function handleSubmit(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   return (
@@ -42,16 +46,28 @@ export default function StarRating({ maxRating = 5 }) {
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onTempHover={() => setTempRating(i + 1)}
             onTempLeave={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={text}>{tempRating || rating || ""}</p>
+      <p style={text}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-// Start component //
-function Star({ onRate, full, onTempHover, onTempLeave }) {
+// Star component //
+function Star({ onRate, full, onTempHover, onTempLeave, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
   return (
     <span
       style={starStyle}
@@ -63,8 +79,8 @@ function Star({ onRate, full, onTempHover, onTempLeave }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={size}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -73,12 +89,12 @@ function Star({ onRate, full, onTempHover, onTempLeave }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="{2}"
+            strokeWidth={2}
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
@@ -115,6 +131,4 @@ EMPTY STAR
     strokeWidth="{2}"
     d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
   />
-</svg>
-
-*/
+</svg>  */
--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRated, setMovieRated] = useState(0);
+  return (
+    <div>
+      <StarRating color="green" maxRating={10} onSetRating={setMovieRated} />
+      <p>This movie was rated {movieRated} star.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "ok", "good", "amazing"]}
+    />
+    <StarRating color="red" size={20} defaultRating={2} />
+    <Test />
   </React.StrictMode>
 );
