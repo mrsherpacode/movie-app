ab1a8b6037b5652a6f7a11a7a764cf4a9c021507
mrsherpacode <sherpacoding@gmail.com>
2025-03-28 14:37:15 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style.css";
+import Apps from "./Expander";
+
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 import { useState } from "react";
 
-function Test() {
-  const [movieRated, setMovieRated] = useState(0);
-  return (
-    <div>
-      <StarRating color="green" maxRating={10} onSetRating={setMovieRated} />
-      <p>This movie was rated {movieRated} star.</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRated, setMovieRated] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="green" maxRating={10} onSetRating={setMovieRated} />
+//       <p>This movie was rated {movieRated} star.</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    {/* <App />
     <StarRating
       maxRating={5}
       messages={["terrible", "bad", "ok", "good", "amazing"]}
     />
     <StarRating color="red" size={20} defaultRating={2} />
-    <Test />
+    <Test /> */}
+    <Apps />
   </React.StrictMode>
 );
