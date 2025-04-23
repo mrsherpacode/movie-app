3f763449337d071791c7575ae1398117915307f7
mrsherpacode <sherpacoding@gmail.com>
2025-04-22 21:22:35 -0500

    dependency array is added

--- src/App.js
+++ src/App.js
@@ -53,51 +53,63 @@ const average = (arr) =>
 ///// API KEY is from this site (https://www.omdbapi.com/)/////
 // here i'm lifting up state
 export default function App() {
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const KEY = "e9c51d8d";
-  const query = "Interstellar";
 
   //  here, i'm using useEffect hook here
   // The function inside useEffect is executed after the component renders for the first time (on mount).
   // It fetches data from the OMDB API using the fetch function.
-  useEffect(function () {
-    async function moviesData() {
-      try {
-        // this API link is from this site (https://www.omdbapi.com/)
-        // this shows loading spinner on the screen before the movie data is fetched.
-        setIsLoading(true);
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-        );
-        //Throwing error when there is problem when fetching data.
-        if (!res.ok) throw new Error("Something went wrong");
-        const data = await res.json();
-        //  Throwing error if the search do not matches the search movies
-        if (data.Response === "False") throw new Error("movies not found ");
-        setMovies(data.Search);
-        // console.log(data.Search);
-        // setIsLoading is false once the data is fetched.
-      } catch (err) {
-        // for catching error.
-        console.error(err.message);
-        setError(err.message);
-      } finally {
-        // finally exicute the code
-        setIsLoading(false);
+  useEffect(
+    function () {
+      async function moviesData() {
+        try {
+          // this API link is from this site (https://www.omdbapi.com/)
+          // this shows loading spinner on the screen before the movie data is fetched.
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
+          //Throwing error when there is problem when fetching data.
+          if (!res.ok) throw new Error("Something went wrong");
+          const data = await res.json();
+          //  Throwing error if the search do not matches the search movies
+          if (data.Response === "False") throw new Error("movies not found ");
+          setMovies(data.Search);
+          // console.log(data.Search);
+          // setIsLoading is false once the data is fetched.
+        } catch (err) {
+          // for catching error.
+          console.error(err.message);
+          setError(err.message);
+        } finally {
+          // finally exicute the code
+          setIsLoading(false);
+        }
       }
-    }
-    // Here, i'm calling moviesData function because its inside another function.
-    moviesData();
-  }, []);
+
+      // if the search queries is less then three words then  make setMovies empty
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      // Here, i'm calling moviesData function because its inside another function.
+      moviesData();
+    },
+    //Here, the query state is a  depedency array and every time this state changes the useEffect hook will execute and re-render the query result.
+    [query]
+  );
   return (
     <>
       {/* logo, search, NumResults are childrens  of NavBar. */}
       <NavBar>
         <Logo />
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
       <Main>
@@ -161,9 +173,7 @@ function Logo() {
   );
 }
 //Search component //
-function Search() {
-  const [query, setQuery] = useState("");
-
+function Search({ query, setQuery }) {
   return (
     <input
       className="search"
